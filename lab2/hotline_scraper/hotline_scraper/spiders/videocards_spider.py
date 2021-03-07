502f9643096dd9191743e03b23d9a14b950cdd53
kbtania1@gmail.com <kbtania1@gmail.com>
2021-03-07 23:17:05 +0200

    Added Lab 2

---
+++ lab2/hotline_scraper/hotline_scraper/spiders/videocards_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+from hotline_scraper.items import HotlineScraperItem
+
+class VideocardsSpider(scrapy.Spider):
+    name = "videocards"
+    start_urls = ['https://hotline.ua/computer/videokarty/']
+
+    def parse(self, response):
+        products = response.css('li.product-item') # all videocards
+        for product in products:
+            try:
+                videocard = HotlineScraperItem()
+                videocard["name"] = product.css('div.item-info p a::text').get().strip('\n').replace('    ', '').rstrip()
+                videocard["price"] = product.css('div.price-md span::text').get().replace('\xa0', ' ')
+                videocard["link"] = 'hotline.ua' + product.css('p.h4 a::attr(href)').get()
+                yield videocard
+            except AttributeError:
+                pass
+        next_page = response.css('a.next ::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(self.start_urls[0]+next_page, callback=self.parse)
+    
---
+++ lab2/library_scraper/library_scraper/spiders/library_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+from library_scraper.items import LibraryScraperItem
+
+class LibrarySpider(scrapy.Spider):
+    name = "library"
+    allowed_domains = ['irbis-nbuv.gov.ua']
+    start_urls = ['http://www.irbis-nbuv.gov.ua/cgi-bin/irbis_nbuv/cgiirbis_64.exe?S21CNR=1000&S21STN=1&S21REF=2&C21COM=S&I21DBN=UJRN&P21DBN=UJRN&S21All=%3C.%3EJJA%3DA$%3C.%3E&S21FMT=j_brief&Z21ID=&S21SRW=nz%27']
+    
+    def get_cities(self, response):
+        books = response.xpath('/html/body/table[1]/tr[4]/td[2]/table[2]/table[2]')
+        to = len(books[0].css('tr td::text'))
+        cities = []
+        for i in range(5, to, 2):
+            cities.append(books[0].css('tr td::text')[i].getall()[0][2:])
+        return cities
+        
+
+    def parse(self, response):
+        books = response.xpath('/html/body/table[1]/tr[4]/td[2]/table[2]/table[2]')
+        count = len(books[0].css('tr td a::attr(href)'))
+        for i in range(count):
+            book = LibraryScraperItem()
+            book["name"] = books[0].css('tr td a::text')[i].getall()[0]
+            book["city"] = self.get_cities(response)[i],
+            book["link"] = books[0].css('tr td a::attr(href)')[i].getall()
+            yield book
+            
+        letters = 'ABCDEFGHIJLMOPRSTUVWАБВГДЕЄЖЗИІКЛМНОПРСТУФХЦЧШЭЮЯ'  # for links
+        
+        for i in range(1, len(letters)):
+            yield response.follow(f'http://www.irbis-nbuv.gov.ua/cgi-bin/irbis_nbuv/cgiirbis_64.exe?S21CNR='
+                                  f'1000&S21STN=1&S21REF=2&C21COM=S&I21DBN=UJRN&P21DBN=UJRN&S21All=%3C.%3EJJA%3D'
+                                  f'{letters[i]}$%3C.%3E&S21FMT=j_brief&Z21ID=&S21SRW=nz', callback=self.parse)
